f81a8a0a16acc667667b5c275fdb9ebcf61a886f
nthssss <nocturn37e@outlook.com>
2018-12-13 21:24:03 +0800

    删掉model.py中多余的一行

--- polls/models.py
+++ polls/models.py
@@ -13,7 +13,6 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
